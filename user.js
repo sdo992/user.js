28b43b84c1f174dbfdc50c36ec8e4e307204cca1
Steven Osborne <46057937+sdo992@users.noreply.github.com>
2026-01-16 15:18:11 -0900

    Update user.js
    Added newer options not previously covered and options to disable AI

--- user.js
+++ user.js
@@ -21,7 +21,9 @@ user_pref("browser.cache.disk.smart_size.enabled", false); //Don't let FF contro
 user_pref("browser.cache.memory.enable", true); // Enable memory cache, http://kb.mozillazine.org/Browser.cache.memory.enable
 user_pref("browser.cache.offline.enable", false); // Disable offline cache
 user_pref("browser.casting.enabled", false); // Part of the UPnP protocol: SSDP disabled
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
+user_pref("browser.discovery.enabled", false); // Disable personalized extension recommendations
 user_pref("browser.display.use_document_fonts", 0); // Prevent font fingerprint
 user_pref("browser.download.alwaysOpenPanel", false); //Disable Downloads popup
 user_pref("browser.download.folderList", 2); // 0 = Desktop, 1 = Downloads folder, 2 = Use most recent
@@ -33,20 +35,24 @@ user_pref("browser.fixup.alternate.enabled", false); // Do not try to guess doma
 user_pref("browser.fixup.hide_user_pass", true);
 user_pref("browser.formfill.enable", false); // Don't autofill
 user_pref("browser.newtab.preload", false); // same
-user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false); // Forget telemetry
-user_pref("browser.newtabpage.activity-stream.telemetry", false); // Forget telemetry
-user_pref("browser.newtabpage.activity-stream.telemetry.ping.endpoint", ""); // Forget telemetry
 user_pref("browser.newtabpage.activity-stream.asrouter.providers.snippets", "");
+user_pref("browser.newtabpage.activity-stream.default.sites", ""); // No default sites, unless you add them yourself
 user_pref("browser.newtabpage.activity-stream.disableSnippets", true);
 user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false); // Forget telemetry
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false); // FF58+, just say 'No'
+user_pref("browser.newtabpage.activity-stream.showSponsoredCheckboxes", false); // Choose to support, or not, Mozilla
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false); // Thanks to @ryandaniels, disable sponsored Top Sites
+user_pref("browser.newtabpage.activity-stream.telemetry", false); // Forget telemetry
+user_pref("browser.newtabpage.activity-stream.telemetry.ping.endpoint", ""); // Forget telemetry
 user_pref("browser.newtabpage.directory.ping", "");
 user_pref("browser.newtabpage.directory.source", "data:text/plain,{}");
 user_pref("browser.newtabpage.enabled", false); // No activity on New Tab
 user_pref("browser.newtabpage.enhanced", false); // Unable to determine if this is still valid
 user_pref("browser.newtab.preload", false); // Quit talking when I don't want you to talk!
 user_pref("browser.offline-apps.notify", true); // Notify when sites offer offline storage
+user_pref("browser.privatebrowsing.forceMediaMemoryCache", true); // Set media cache in Private Browsing to in-memory, size set below; Firefox 75+
 user_pref("browser.safebrowsing.malware.enabled", false); // Begin [Disable Safebrowsing] Don't send sites to Mozilla or Google
 user_pref("browser.safebrowsing.phishing.enabled", false);
 user_pref("browser.safebrowsing.downloads.enabled", false);
@@ -78,21 +84,45 @@ user_pref("browser.search.region", "US"); // US as default location
 user_pref("browser.search.suggest.enabled", false); //Disable search suggestions in the search bar
 user_pref("browser.search.update", false); // Don't update search engines automatically
 user_pref("browser.selfsupport.url", ""); // Don't send a heartbeat ping
-user_pref("browser.sessionstore.interval", "3600000"); //Don't save so often
 user_pref("browser.send_pings", false); // "The attribute would be useful for letting websites track visitors’ clicks. " https://wiki.mozilla.org/Privacy/Privacy_Task_Force/firefox_about_config_privacy_tweeks
-user_pref("browser.sessionstore.max_tabs_undo", 0); // Disable temporary list of recently closed tabs 
+user_pref("browser.sessionstore.interval", "3600000"); //Don't save so often
+user_pref("browser.sessionstore.max_tabs_undo", 0); // Disable temporary list of recently closed tabs
+user_pref("browser.sessionstore.privacy_level", 2); // 0 = Everywhere, 1 = Encrypted sites only, 2 = nowhere
 user_pref("browser.shell.checkDefaultBrowser", false); // Don't check & shut up
 user_pref("browser.startup.homepage", "about:blank"); //Blank start page
 user_pref("browser.tabs.crashReporting.sendReport", false); // Don't send crash report
 user_pref("browser.uitour.enabled", false); // Disable tour
+user_pref("browser.urlbar.addons.featureGate", false); // Firefox 115+
+user_pref("browser.urlbar.amp.featureGate", false); // Firefox Ad-Marketplace, Firefox 141+
+user_pref("browser.urlbar.fakespot.featureGate", false); // Firefox 130+
 user_pref("browser.urlbar.autocomplete.enabled", false); // Kind of like prefetch
 user_pref("browser.urlbar.autoFill", false); // Don't autofill in the URL bar
 user_pref("browser.urlbar.autoFill.typed", false); // Ditto
 user_pref("browser.urlbar.filter.javascript", true); // Do not let the URL bar display JS in history
+user_pref("browser.urlbar.mdn.featureGate", false); // Firefox 117+
+user_pref("browser.urlbar.quicksuggest.enabled", false); // Firefox 92+
 user_pref("browser.urlbar.speculativeConnect.enabled", false); // Disable preloading of autocomplete URLs https://wiki.mozilla.org/Privacy/Privacy_Task_Force/firefox_about_config_privacy_tweeks
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false); // Firefox 95+
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
 user_pref("browser.urlbar.suggest.searches",false); // Don't suggest anything
 user_pref("browser.urlbar.suggest.history", false); // Don't keep history of suggestions
+user_pref("browser.urlbar.trending.featureGate", false); // Don't suggest trending searches
 user_pref("browser.urlbar.trimURLS", false); // Do not trim HTTP off URLs in the address bar
+user_pref("browser.urlbar.weather.featureGate", false); // I don't want Firefox using geoclue2
+user_pref("browser.urlbar.wikipedia.featureGate", false); // Firefox 141+
+user_pref("browser.urlbar.yelp.featureGate", false); // Firefox 124+
+user_pref("browser.xul.error_pages.expert_bad_cert", true); // Advanced info on Insecure Warning pages
+
+// AI Specific
+user_pref("browser.ml.enable", false); // I do not ever want an AI browser
+user_pref("browser.ml.chat.enabled", false); // If I want to chat with AI, I will do it on my own terms
+user_pref("browser.ml.chat.page.footerBadge", false);
+user_pref("browser.ml.chat.page.menuBadge", false);
+user_pref("browser.ml.linkPreview.enabled", false); // I don't need AI watching every website I visit
+user_pref("browser.tabs.groups.smart.enabled", false); // Stop AI from creating tab groups
+user_pref("browser.tabs.groups.smart.userEnabled", false); // More AI slop
+user_pref("extensions.ml.enabled", false); // Just no, it's getting tiresome now
+user_pref("sidebar.notification.badge.aichat", false); // *sigh*
 
 // Dev Tools
 user_pref("devtools.chrome.enabled", false); // Disable dev tools in browser
@@ -122,12 +152,20 @@ user_pref("dom.network.enabled", false); // See: // https://www.torproject.org/p
 //user_pref("dom.push.connection.enabled", false);
 //user_pref("dom.push.serverURL", "");
 //user_pref("dom.push.userAgentID", "");
+user_pref("dom.security.https_only_mode_send_http_background_request", false); // Stop from sending hidden ping after 3 seconds, Firefox 82+
 user_pref("dom.serviceWorkers.enabled", false); //Breaks functionality on some sites, such as Google Street View, and breaks Firefox sync
 user_pref("dom.vr.enabled", false); // I don't want VR on my web browser
 user_pref("dom.webnotifications.enabled", false);
 user_pref("dom.workers.enabled", false);
 user_pref("dom.webaudio.enabled", false);
 user_pref("dom.telephony.enabled", false); // https://wiki.mozilla.org/WebAPI/Security/WebTelephony
+
+// Extensions
+user_pref("extensions.blocklist.enabled", false);
+user_pref("extensions.blocklist.url", "");
+user_pref("extensions.getAddons.showPane", false); // Hidden Preference
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false); // Disable recommendations in about:addons
+user_pref("extensions.pocket.enabled", false); // Fuck pocket
 
 // GEO-location
 user_pref("geo.enabled", false);
@@ -144,6 +182,7 @@ user_pref("media.gmp-gmpopenh264.enabled", false); // Disable automatic download
 user_pref("media.gmp-manager.url", ""); // Same as above
 user_pref("media.navigator.enabled", false); // Sites cannot access media devices
 user_pref("media.navigator.video.enabled", false); // No access to video devices
+user_pref("media.memory_cache_max_size", 65536); // See pref: browser.privatebrowsing.forceMediaMemoryCache above, Firefox 75+
 user_pref("media.webspeech.synth.enabled", false); // No webspeech synthesis
 user_pref("media.webspeech.recognition.enable", false); // No webspeech recognition
 user_pref("media.peerconnection.enabled", false); // Disable WebRTC completely, see: https://wiki.mozilla.org/Media/WebRTC/Privacy
@@ -167,16 +206,14 @@ user_pref("network.manage-offline-status",  false); // Keep your paws off my con
 user_pref("network.http.speculative-parallel-limit", 0); // Completely disable speculative pre-connections
 
 // Miscellaneous
-user_pref("app.normandy.enabled", false); // Fuck telemetry pings
+user_pref("app.normandy.enabled", false); // Forget telemetry pings
 user_pref("app.normandy.api_url", ""); // Explicitly set URL to nothing for telemetry
 user_pref("app.shield.optoutstudies.enabled", false); // Opt-out of studies, no effect if Health Reports are disabled
 user_pref("beacon.enabled", false); // See: https://developer.mozilla.org/en-US/docs/Web/API/navigator.sendBeacon
 user_pref("breakpad.reportURL", ""); // Don't report URL when things break
+user_pref("captivedetect.canonicalURL", ""); // See: https://www.eff.org/deeplinks/2017/08/how-captive-portals-interfere-wireless-security-and-privacy
 user_pref("clipboard.autocopy", false); // Linux only: Don't send items to clipboard
 user_pref("device.sensors.enabled", false); // Disable the sensor API, see: https://wiki.mozilla.org/Sensor_API
-user_pref("extensions.blocklist.enabled", false);
-user_pref("extensions.blocklist.url", "");
-user_pref("extensions.pocket.enabled", false); // Fuck pocket
 user_pref("gfx.font_rendering.opentype_svg.enabled", false); // Disable SVG in OpenType fonts, see: https://wiki.mozilla.org/SVGOpenTypeFonts
 user_pref("javascript.options.asmjs",   false); // Disable asm.js, see: http://asmjs.org/
 user_pref("keyword.enabled", false); // Don't leak typed information to default search engine
@@ -261,5 +298,6 @@ user_pref("print.print_headerright", "");
 user_pref("print.print_footercenter", "");
 user_pref("print.print_footerleft", "");
 user_pref("print.print_footerright", "");
+
 
 
